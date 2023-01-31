7b8292631ba51a7a66f5f5172b430e5ae1c3b5c7
todimu <todimuisewon@gmail.com>
2023-01-31 07:51:06 +0100

    added a db to the project

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"github.com/plurasight/webservice/database"
 	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/plurasight/webservice/product"
 )
 
 const basePath = "/api"
 
 func main() {
+	database.SetupDatabase()
 	product.SetupRoutes(basePath)
 	err := http.ListenAndServe(":5001", nil)
 	if err != nil {
